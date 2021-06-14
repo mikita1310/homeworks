fa7c99530f181dc2f71092b72a7f5a68aec4e107
mikita1310 <85647297+mikita1310@users.noreply.github.com>
2021-06-14 17:45:39 +0300

    Update javasrcitp.js

--- A2+ Написать чистую функцию/js/javasrcitp.js
+++ A2+ Написать чистую функцию/js/javasrcitp.js
@@ -1,13 +1,22 @@
 var str = prompt("Введите любое значение");
 function trim(str) {
-  for (k = 0; k < str.length; k++) {
-    if (str.charAt(k, str.length-1) !== " ") {
-      return str.slice(k, str.length-1);
+  let v, d;
+  for (let k = 0; k < str.length; k++) {
+    if (str.charAt(k) !== " ") {
+      v = str.slice(k);
+      break;
     }
   }
+  d = v;
+  for (let k = v.length - 1; k >= 0; k--) {
+    if (v.charAt(k) !== " ") {
+      d = v.slice(0, k + 1);
+      break;
+    }
+  }
+  return d;
 }
 
 var ost = trim(str);
 
 alert(`${"<"}` + ost + `${">"}`);
-
