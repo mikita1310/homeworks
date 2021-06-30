cf9fcd43cecb15f98f4cf9bc61f921b475bde355
mikita1310 <85647297+mikita1310@users.noreply.github.com>
2021-06-30 19:21:57 +0300

    Update javasrcitp.js

--- A2+ Написать чистую функцию/js/javasrcitp.js
+++ A2+ Написать чистую функцию/js/javasrcitp.js
@@ -1,20 +1,29 @@
-var str = prompt("Введите любое значение");
-function trim(str) {
-  let v, d;
-  for (let k = 0; k < str.length; k++) {
-    if (str.charAt(k) !== " ") {
-      v = str.slice(k);
-      break;
+let str = prompt("Введите любое значение");
+function trim(x) {
+  let l = 0,
+      r = str.length - 1;
+
+  if (str[l] !== " " && str[r] !== " ") {
+    console.log("no spaces");
+    return str;
+  } else {
+    for (let i = l; i <= r; i++) {
+      if (str[i] === " ") l++;
+      else break;
+    }
+
+    for (let q = r; q >= 0; q--) {
+      if (str[r] === " ") r--;
+    }
+    
+    if (l > 0 && r < 0) {
+      console.log("only spaces");
+      return (str = "");
+    } else {
+      console.log("spaces was deleted")
+      return str.slice(l, r + 1);
     }
   }
-  d = v;
-  for (let k = v.length - 1; k >= 0; k--) {
-    if (v.charAt(k) !== " ") {
-      d = v.slice(0, k + 1);
-      break;
-    }
-  }
-  return d;
 }
 
 var ost = trim(str);
